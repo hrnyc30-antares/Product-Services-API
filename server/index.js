6773a71f8e8507ea95aa67c1c3217f14e2d0e144
Jose Collado <jcoll1020@gmail.com>
2020-07-24 16:24:45 -0400

    routing configuration completed along with db query and response object format completed

--- server/index.js
+++ server/index.js
@@ -1,34 +1,118 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose')
 const initOptions = {
   error(error, e) {
     if (e.cn) {
       console.log('CN:', e.cn);
       console.log('EVENT:', error.message || error);
     }
-  }
+  },
 };
-const pgp = require('pg-promise')(initOptions)
+const pgp = require('pg-promise')(initOptions);
 
-mongoose.connect("mongodb://mongo:27017/products-api", { useNewUrlParser: true })
-.then(() => console.log('connected to mongo'))
-.catch((err) => console.log(err))
-
-pgp('postgresql://postgres:postgres@pg:5432/products-api')
-.connect()
-.then(() => console.log('connected to postgres'))
-.catch((error) => {
-  console.log('error message --> ', error)
-})
-
+let db = pgp('postgresql://postgres:postgres@pg:5432/products-api');
 
 app.use(cors());
 
+app.get('/products/list', (req, res) => {
+  db.any('select * from products_info limit 5')
+    .then((data) => res.status(200).send(JSON.parse(JSON.stringify(data))))
+    .catch((err) => {
+      console.log('error -->', err);
+      res.sendStatus(500);
+    });
+});
 
-app.get('/', (req, res) => {
-  res.send('hello from the server !');
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+  db.any(`select * from products_info where id = ${id}`)
+    .then((data) => {
+      db.any(`select * from features where id = ${id}`)
+        .then((featureData) => {
+          featureData.forEach((el) => delete el.id);
+          data[0]['features'] = featureData;
+          res.status(200).send(JSON.parse(JSON.stringify(data[0])));
+        })
+        .catch((error) => console.log('error-->', error));
+    })
+    .catch((error) => {
+      console.log('error message -->', error);
+      res.sendStatus(500);
+    });
+});
+
+app.get('/products/:id/styles', (req, res) => {
+  const id = req.params.id;
+  let result = {
+    product_id: id,
+    results: []
+  }
+
+//=============================================
+//query call for styles  
+  db.any(`select * from styles where product_id = ${id}`)
+  .then(data => {
+    data.forEach((el) => {
+      if(el.sale_price === 'null') el.sale_price = "0"
+      delete el.product_id
+//====================================================
+      //query call for photos
+      db.any(`select * from photos where style_id = ${el.style_id}`)
+      .then(photoData => {
+        photoData.forEach(val => delete val.style_id)
+        if(photoData.length === 0){
+          photoData = [
+            {
+              "thumbnail_url": null,
+              "url": null
+            }
+          ]
+        }
+        el['photos'] = photoData
+//=====================================================
+        //query call for skus
+        db.any(`select * from skus where style_id = ${el.style_id}`)
+        .then(skuData => {
+          let skuObj = {}
+          skuData.forEach(value => skuObj[value.size] = value.qty)
+          if(skuData.length === 0){
+            skuObj['null'] = null
+          }
+          el['skus'] = skuObj
+
+//=======================================================          
+          result.results.push(el)
+          if(result.results.length === data.length){
+            result.results.sort((a,b) => a.style_id - b.style_id)
+            res.status(200).send(JSON.parse(JSON.stringify(result)))
+          }
+        })
+        .catch(error => console.log('skus error -->', error))
+      })
+      .catch(error => console.log('photos error -->', error))
+    })
+  })
+  .catch(error => {
+    console.log('styles error message --> ', error)
+    res.sendStatus(500)
+  })
+});
+
+app.get('/products/:id/related', (req, res) => {
+  const id = req.params.id;
+  let results = [];
+  db.any(`SELECT * FROM related WHERE current_id = ${id}`)
+    .then((data) => {
+      data.forEach((el) => {
+        results.push(el.related_id);
+      });
+      res.status(200).send(JSON.parse(JSON.stringify(results)));
+    })
+    .catch((error) => {
+      console.log('error -->', error);
+      res.sendStatus(500);
+    });
 });
 
 const port = 3001;
